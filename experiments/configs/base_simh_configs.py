9e151439f3a60e907d8ff3e136db561e39ae3ff5
Joost Berkhout <joost.berkhout@vu.nl>
2025-06-15 11:42:57 +0200

    Parameter tuning simulate last heuristic

--- experiments/configs/base_simh_configs.py
+++ experiments/configs/base_simh_configs.py
@@ -1,21 +1,62 @@
+from experiments.configs.base_exp_config import exp_config
 from experiments.utils.SimheuristicSpec import SimheuristicSpec
 from simpyjobshop.simheuristics import (
-    DeterministicOptimizationConfig,
-    deterministic_optimization,
+    SimulateLastSolutionsConfig,
+    simulate_last_solutions,
 )
 
 # Simheuristic configurations
-det_opt_conf_1: DeterministicOptimizationConfig = {
+# det_opt_conf_1: DeterministicOptimizationConfig = {
+#     "det_repr": "mean",
+# }
+# det_opt_conf_2: DeterministicOptimizationConfig = {
+#     "det_repr": 0.6,
+# }
+# det_opt_conf_3: DeterministicOptimizationConfig = {
+#     "det_repr": 0.7,
+# }
+# det_opt_conf_4: DeterministicOptimizationConfig = {
+#     "det_repr": 0.8,
+# }
+sim_last_config_1: SimulateLastSolutionsConfig = {
     "det_repr": "mean",
+    "max_size_elite_set": 5,
+    "frac_budget_final_elites_sim": 30 / exp_config["time_limit"],
 }
-det_opt_conf_2: DeterministicOptimizationConfig = {
+sim_last_config_2: SimulateLastSolutionsConfig = {
+    "det_repr": "mean",
+    "max_size_elite_set": 10,
+    "frac_budget_final_elites_sim": 30 / exp_config["time_limit"],
+}
+sim_last_config_3: SimulateLastSolutionsConfig = {
     "det_repr": 0.6,
+    "max_size_elite_set": 5,
+    "frac_budget_final_elites_sim": 30 / exp_config["time_limit"],
 }
-det_opt_conf_3: DeterministicOptimizationConfig = {
+sim_last_config_4: SimulateLastSolutionsConfig = {
+    "det_repr": 0.6,
+    "max_size_elite_set": 10,
+    "frac_budget_final_elites_sim": 30 / exp_config["time_limit"],
+}
+sim_last_config_5: SimulateLastSolutionsConfig = {
     "det_repr": 0.7,
+    "max_size_elite_set": 5,
+    "frac_budget_final_elites_sim": 30 / exp_config["time_limit"],
 }
-det_opt_conf_4: DeterministicOptimizationConfig = {
+sim_last_config_6: SimulateLastSolutionsConfig = {
+    "det_repr": 0.7,
+    "max_size_elite_set": 10,
+    "frac_budget_final_elites_sim": 30 / exp_config["time_limit"],
+}
+sim_last_config_7: SimulateLastSolutionsConfig = {
     "det_repr": 0.8,
+    "max_size_elite_set": 5,
+    "frac_budget_final_elites_sim": 30 / exp_config["time_limit"],
+}
+sim_last_config_8: SimulateLastSolutionsConfig = {
+    "det_repr": 0.8,
+    "max_size_elite_set": 10,
+    "frac_budget_final_elites_sim": 30 / exp_config["time_limit"],
 }
 # stand_simh_config: StandardSimheuristicConfig = {
 #     "det_repr": "mean",
@@ -37,18 +78,17 @@ det_opt_conf_4: DeterministicOptimizationConfig = {
 #     "frac_budget_final_elites_sim": 30 / exp_config["time_limit"],
 #     # frac_budget_final_elites_sim should be enough to sim. final
 # }
-# sim_last_config: SimulateLastSolutionsConfig = {
-#     "det_repr": "mean",
-#     "max_size_elite_set": 5,
-#     "frac_budget_final_elites_sim": 30 / exp_config["time_limit"],
-# }
 
 simheuristics: list[SimheuristicSpec] = [
-    SimheuristicSpec("det_opt_1", deterministic_optimization, det_opt_conf_1),
-    SimheuristicSpec("det_opt_2", deterministic_optimization, det_opt_conf_2),
-    SimheuristicSpec("det_opt_3", deterministic_optimization, det_opt_conf_3),
-    SimheuristicSpec("det_opt_4", deterministic_optimization, det_opt_conf_4),
-    # SimheuristicSpec("sim_last", simulate_last_solutions, sim_last_config),
+    # SimheuristicSpec("det_opt", deterministic_optimization, det_opt_conf),
+    SimheuristicSpec("sim_last", simulate_last_solutions, sim_last_config_1),
+    SimheuristicSpec("sim_last", simulate_last_solutions, sim_last_config_2),
+    SimheuristicSpec("sim_last", simulate_last_solutions, sim_last_config_3),
+    SimheuristicSpec("sim_last", simulate_last_solutions, sim_last_config_4),
+    SimheuristicSpec("sim_last", simulate_last_solutions, sim_last_config_5),
+    SimheuristicSpec("sim_last", simulate_last_solutions, sim_last_config_6),
+    SimheuristicSpec("sim_last", simulate_last_solutions, sim_last_config_7),
+    SimheuristicSpec("sim_last", simulate_last_solutions, sim_last_config_8),
     # SimheuristicSpec("std_simh", standard_simheuristic, stand_simh_config),
     # SimheuristicSpec("dyn_simh", dynamic_simheuristic, dyn_simh_config),
 ]
